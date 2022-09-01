3089542b5b5691aef669c09c46b54e373d11c901
fabianeromano <fabian.ed.romano@gmail.com>
2022-09-01 17:39:11 -0300

    Agregand nuevo desafio

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import { productsRepository } from "./src/db/productos.js";
+import http from "http";
+import { Server as IOServer } from "socket.io";
+import { fileHandler } from "./src/utils/files.js";
 
 const app = express();
+const httpServer = http.createServer(app);
+const io = new IOServer(httpServer);
+const productos = [];
+const messages = [];
 
 app.engine("handlebars", engine());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "handlebars");
 //app.set("view engine", "pug");
 //app.set("view engine", "ejs");
@@ -17,18 +24,41 @@ app.set("views", "./src/views");
 app.get("/", (req, res) => {
   res.render("home");
 });
+app.get("/message-center", (req, res) => {
+  res.render("message-center");
+});
+/* 
 app.post("/productos", (req, res) => {
   const { title, price, thumbnail } = req.body;
   productsRepository.save({ title, price, thumbnail });
   res.render("home");
-});
+}); */
+
 app.get("/productos", (req, res) => {
-  const productos = productsRepository.getAll();
   res.render("productos", { productos, tieneProductos: productos.length > 0 });
+});
+app.get("/messages", (req, res) => {
+  res.render("messages", { messages, hasMessages: messages.length > 0  });
 });
 
 const PORT = process.env.PORT || 8080;
 
-const server = app.listen(PORT, () => {
+const server = httpServer.listen(PORT, () => {
   console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("Usuario conectado");
+
+  socket.emit("productos", productos);
+  socket.emit("messages", messages);
+
+  socket.on("new-product", (data) => {
+    productos.push(data);
+    io.sockets.emit("productos", productos);
+  });
+  socket.on("new-message", (data) => {
+    messages.push(data);
+    io.sockets.emit("messages", messages);
+  });
 });
